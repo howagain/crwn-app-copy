7f58f04c0a06cb6d43bc95466a22f45199314810
Jacob Haugen <howagain@gmail.com>
2020-05-04 15:40:09 -0400

    menu made of mapped subcomponents and added styles

---
+++ src/Pages/Homepage/Homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Homepage.styles.scss'
+
+import Directory from "../../Components/Directory/Directory.component"
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <Directory/>
+        </div>
+    )
+}
+
+export default Homepage
