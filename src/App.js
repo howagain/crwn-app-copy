fa9e0d7e871de95fd8debc89e7c0cc4e40796bb0
Jacob Haugen <howagain@gmail.com>
2020-05-14 01:19:54 -0400

    header added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import Homepage from "./Pages/Homepage/Homepage.component"
 import ShopPage from "./Pages/ShopPage/ShopPage.component"
-
+import Header from "./Components/Header/Header.component"
 
 import {
   Switch, Route
@@ -12,6 +12,7 @@ import {
 export default function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage}/>
         <Route exact path="/shop" component={ShopPage}/>
